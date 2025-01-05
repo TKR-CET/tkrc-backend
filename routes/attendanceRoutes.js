05ab5d8e9dddfadaef73275127ff8f8f48953d84
TKR-CET <vinaybyagari07@gmail.com>
2025-01-06 01:03:25 +0530

    Update attendanceRoutes.js

--- routes/attendanceRoutes.js
+++ routes/attendanceRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { markAttendance, fetchAttendance, checkAttendance } = require("../controllers/attendanceController");
+const { markAttendance, fetchAttendance, checkAttendance, updateAttendance } = require("../controllers/attendanceController");
 
 const router = express.Router();
 
 router.post("/mark-attendance", markAttendance);
 router.get("/fetch-attendance", fetchAttendance);
 router.get("/check", checkAttendance);
+router.put("/update-attendance", updateAttendance); // Adding the update route
 
 module.exports = router;
