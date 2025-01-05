89e4505c8b214b982704ca5527fc997c1cf90419
TKR-CET <vinaybyagari07@gmail.com>
2025-01-05 12:55:55 +0530

    Update attendanceRoutes.js

--- routes/attendanceRoutes.js
+++ routes/attendanceRoutes.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const { markAttendance, fetchAttendance } = require("../controllers/attendanceController");
+const {
+  markAttendance,
+  fetchAttendance,
+  checkMarkedAttendance,
+} = require("../controllers/attendanceController");
 
 // Route to mark attendance
 router.post("/mark-attendance", markAttendance);
 
 // Route to fetch attendance by date
 router.get("/fetch-attendance", fetchAttendance);
+
+// Route to check marked periods by date
+router.get("/check", checkMarkedAttendance);
 
 module.exports = router;
