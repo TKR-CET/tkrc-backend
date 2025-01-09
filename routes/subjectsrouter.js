80e8a6e8bd6846c03bb2f05858e030f658825994
TKR-CET <vinaybyagari07@gmail.com>
2025-01-09 13:48:09 +0530

    Update subjectsrouter.js

--- routes/subjectsrouter.js
+++ routes/subjectsrouter.js
@@ -3,14 +3,14 @@ const router = express.Router();
 const yearController = require('../controllers/subjectscontroller'); // Adjust path
 
 // CRUD operations for years
-router.get('/year-subjects', yearController.getAllYears);
-router.get('/year-subjects/:id', yearController.getYearById);
-router.post('/year-subjects', yearController.addYear);
-router.put('/year-subjects/:id', yearController.updateYear);
-router.delete('/year-subjects/:id', yearController.deleteYear);
+router.get('/get-years', yearController.getAllYears);
+router.get('/year/:id', yearController.getYearById);
+router.post('/add-year', yearController.addYear);
+router.put('/update-year/:id', yearController.updateYear);
+router.delete('/delete-year/:id', yearController.deleteYear);
 
 // Add department and subject
-router.post('/year-subjects/:yearId/departments', yearController.addDepartmentToYear);
-router.post('/year-subjects/:yearId/departments/:departmentId/subjects', yearController.addSubjectToDepartment);
+router.post('/add-department/:yearId/departments', yearController.addDepartmentToYear);
+router.post('/add-subject/:yearId/departments/:departmentId/subjects', yearController.addSubjectToDepartment);
 
 module.exports = router;
