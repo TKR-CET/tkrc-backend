21590364082b08c8ff65d0b6b9c9856e93e6f2e9
TKR-CET <vinaybyagari07@gmail.com>
2025-01-09 11:17:30 +0530

    Create facultyrouter.js

---
+++ routes/facultyrouter.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const {
+  addFaculty,
+  getAllFaculty,
+  getFacultyById,
+  updateFaculty,
+  deleteFaculty,
+} = require("../controllers/facultycontroller"); // Path to the controller
+
+const router = express.Router();
+
+router.post("/addfaculty", addFaculty); // Add new faculty
+router.get("/getfaculty", getAllFaculty); // Get all faculty
+router.get("/:id", getFacultyById); // Get faculty by ID
+router.put("/:id", updateFaculty); // Update faculty by ID
+router.delete("/:id", deleteFaculty); // Delete faculty by ID
+
+module.exports = router;
