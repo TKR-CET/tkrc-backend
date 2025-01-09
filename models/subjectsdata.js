b99d7e334bcb49d8ffac5e48d5d4f08433b31b79
TKR-CET <vinaybyagari07@gmail.com>
2025-01-09 11:59:34 +0530

    Update subjectsdata.js

--- models/subjectsdata.js
+++ models/subjectsdata.js
@@ -1,1 +1,24 @@
+const mongoose = require('mongoose');
+
+// Subject Schema
+const SubjectSchema = new mongoose.Schema({
+  name: { type: String, required: true }, // Subject Name
+});
+
+// Department Schema
+const DepartmentSchema = new mongoose.Schema({
+  name: { type: String, required: true }, // Department Name (CSE, ECE, etc.)
+  subjects: [SubjectSchema], // Array of Subjects
+});
+
+// Year Schema
+const YearSchema = new mongoose.Schema({
+  year: { type: String, required: true }, // Year Name (1, 2, 3, 4)
+  departments: [DepartmentSchema], // Array of Departments
+});
+
+// Main Model
+const YearModel = mongoose.model('Year', YearSchema);
+
+module.exports = YearModel;
 
