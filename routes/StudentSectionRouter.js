771aadb3fb61b9da7d7cab5d4b66a5bb63f5ba1e
TKR-CET <vinaybyagari07@gmail.com>
2025-01-10 12:28:46 +0530

    Update StudentSectionRouter.js

--- routes/StudentSectionRouter.js
+++ routes/StudentSectionRouter.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const {
   getStudentsBySection,
-  addStudentToSection,
+  addStudentToSection, 
   updateSectionTimetable,
 } = require("../controllers/StudentSectionController");
 
