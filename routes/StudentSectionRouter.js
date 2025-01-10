f1e34ecf10d29e75c74ef389c17d1350df9f5b73
TKR-CET <vinaybyagari07@gmail.com>
2025-01-10 11:52:10 +0530

    Create StudentSectionRouter.js

---
+++ routes/StudentSectionRouter.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const {
+  getStudentsBySection,
+  addStudentToSection,
+  updateSectionTimetable,
+} = require("../controllers/yearController");
+
+const router = express.Router();
+
+router.get("/:yearId/:departmentId/:sectionId/students", getStudentsBySection); // Get students in a section
+router.post("/:yearId/:departmentId/:sectionId/students", addStudentToSection); // Add a student to a section
+router.put("/:yearId/:departmentId/:sectionId/timetable", updateSectionTimetable); // Update section timetable
+
+module.exports = router;
