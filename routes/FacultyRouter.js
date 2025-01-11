fd710a2156b16bd507287321b7e4dfa7be7c2c25
TKR-CET <vinaybyagari07@gmail.com>
2025-01-11 23:24:48 +0530

    Update FacultyRouter.js

--- routes/FacultyRouter.js
+++ routes/FacultyRouter.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const multer = require("multer");
-const path = require("path");
+
 const {   
   addFaculty, 
   getAllFaculty,
@@ -13,22 +13,14 @@ const {
 } = require("../controllers/FacultyController");
 
 const router = express.Router();
-// Set up Multer again for this file (if not already imported in server.js)
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "./uploads"); // Destination folder
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname)); // File name
-  },
-});
+// Add faculty with image upload
+router.post("/addfaculty", upload.single("image"), addFaculty);
 
-const upload = multer({ storage: storage });
+// Update faculty with image upload
+router.put("/:id", upload.single("image"), updateFaculty);
 
-router.post("/addfaculty", upload.single("image"), addFaculty);// Add new faculty
 router.get("/getfaculty", getAllFaculty); // Get all faculty
-router.get("/:id", getFacultyById); // Get a faculty by ID
-router.put("/:id", updateFaculty); // Update faculty
+router.get("/:id", getFacultyById); // Get a faculty by ID/ Update faculty
 router.delete("/:id", deleteFaculty); // Delete faculty
 router.get("/:id/timetable", getFacultyTimetable); // Get faculty timetable
 router.put("/update/:id/timetable", updateFacultyTimetable); // Update faculty timetable
