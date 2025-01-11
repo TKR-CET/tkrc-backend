72693b51e2d7c594f8641ac283302adc6e8d3867
TKR-CET <vinaybyagari07@gmail.com>
2025-01-11 22:19:30 +0530

    Update FacultyRouter.js

--- routes/FacultyRouter.js
+++ routes/FacultyRouter.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const multer = require("multer");
+const path = require("path");
 const {  
   addFaculty, 
   getAllFaculty,
@@ -11,6 +13,17 @@ const {
 } = require("../controllers/FacultyController");
 
 const router = express.Router();
+// Set up Multer again for this file (if not already imported in server.js)
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./uploads"); // Destination folder
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname)); // File name
+  },
+});
+
+const upload = multer({ storage: storage });
 
 router.post("/addfaculty", upload.single("image"), addFaculty);// Add new faculty
 router.get("/getfaculty", getAllFaculty); // Get all faculty
