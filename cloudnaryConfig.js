f3f31b7876f9897d3b84f4721cb42d91a5148d85
TKR-CET <vinaybyagari07@gmail.com>
2025-01-16 21:29:07 +0530

    Update cloudnaryConfig.js

--- cloudnaryConfig.js
+++ cloudnaryConfig.js
@@ -1,1 +1,10 @@
+const cloudinary = require("cloudinary").v2;
 
+// Configure Cloudinary
+cloudinary.config({
+  cloud_name: "dppiuypop", // Replace with your Cloudinary cloud name
+  api_key: "412712715735329",       // Replace with your Cloudinary API key
+  api_secret: "m04IUY0-awwtr4YoS-1xvxOOIzU", // Replace with your Cloudinary API secret
+});
+
+module.exports = cloudinary;
